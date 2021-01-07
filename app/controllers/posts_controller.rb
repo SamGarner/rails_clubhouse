f4e34cda88c07ad5b055f40a2bbdf492c6c6a99b
SamGarner <samuel.w.garner@gmail.com>
2021-01-06 18:08:36 -0600

    Fix: Correct posts #create action and index view.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(whitelisted_post_params)
+    @post = Post.new(body: params[:post][:body])
     if @post.save
-      redirect_to root
+      redirect_to root_path
     else
       render :new
     end
